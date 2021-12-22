9adcdce89eb549b20cb64f0e9e755acb063b08e2
Thomas Maulon <tmaulon@betomorrow.com>
2021-12-22 22:27:04 +0100

    structure the entire app with components, services, models and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,84 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { BookFormComponent } from './components/book/book-form/book-form.component';
+import { BookItemComponent } from './components/book/book-item/book-item.component';
+import { BooksListComponent } from './components/book/books-list/books-list.component';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four/four-oh-four.component';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome/welcome.component';
+import { AuthGuardService } from './services/auth-guard/auth-guard.service';
+import { AuthService } from './services/auth/auth.service';
+import { BookService } from './services/book/book.service';
+
+export enum AppRoutes {
+  home = '',
+  signin = 'auth/signin',
+  signup = 'auth/signup',
+  books = 'livres',
+  newBook = 'nouveau-livre',
+  fourOhFour = 'not-found',
+}
+
+const appRoutes: Routes = [
+  {
+    path: AppRoutes.home,
+    component: WelcomeComponent,
+  },
+  {
+    path: AppRoutes.signin,
+    // canActivate: [AuthGuardService],
+    component: SigninComponent,
+  },
+  {
+    path: AppRoutes.signup,
+    // canActivate: [AuthGuardService],
+    component: SignupComponent,
+  },
+  {
+    path: `${AppRoutes.books}`,
+    // canActivate: [AuthGuardService],
+    component: BooksListComponent,
+  },
+  {
+    path: `${AppRoutes.books}/:id`,
+    // canActivate: [AuthGuardService],
+    component: BookItemComponent,
+  },
+  {
+    path: `${AppRoutes.newBook}`,
+    // canActivate: [AuthGuardService],
+    component: BookFormComponent,
+  },
+  { path: AppRoutes.fourOhFour, component: FourOhFourComponent },
+  { path: '**', redirectTo: AppRoutes.fourOhFour },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BooksListComponent,
+    BookItemComponent,
+    BookFormComponent,
+    HeaderComponent,
+    FourOhFourComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService, AuthGuardService, BookService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/book/book.model.ts
@@ -0,0 +1,5 @@
+export class Book {
+  picture: string = '';
+  resume: string = '';
+  constructor(public title: string, public author: string) {}
+}
